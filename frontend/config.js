650d8a628a52462fa0496f1bf3b65ddaa74515f6
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-07-04 20:29:23 +0200

    :wrench: Point frontend to real production API

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
 export const apiUrlDev = `http://localhost:4444`
-export const apiUrlProd = `https://lucsali-sf-yoga-prod.herokuapp.com/`
+export const apiUrlProd = `https://kuraitis-yoga-prod.herokuapp.com/`
