b41744f8962a0b346f0efa97f8b0139482eca3c8
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-07-05 20:59:55 +0200

    :sparkles: Visualize active route in navigation

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { useRouter } from 'next/router'
+import React from 'react'
 import Link from 'next/link'
 
 const StyledNav = styled.div`
@@ -16,28 +18,43 @@ const StyledNav = styled.div`
 		&:hover {
 			color: ${props => props.theme.colors.darkGray};
 		}
+
+		&.selected {
+			color: ${props => props.theme.colors.blue};
+		}
 	}
 `
+const NavLink = ({ href, children }) => {
+	const router = useRouter()
+
+	let className = children.props.className || ''
+	if (router.pathname === href) {
+		className = `${className} selected`
+	}
+
+	return <Link href={href}>{React.cloneElement(children, { className })}</Link>
+}
+
 const Nav = ({ className, children }) => (
 	<StyledNav className={className}>
-		<Link href="/">
+		<NavLink href="/">
 			<a>Home</a>
-		</Link>
-		<Link href="/products">
+		</NavLink>
+		<NavLink href="/products">
 			<a>Products</a>
-		</Link>
-		<Link href="/">
+		</NavLink>
+		<NavLink href="/about">
 			<a>About</a>
-		</Link>
-		<Link href="/">
+		</NavLink>
+		<NavLink href="/markets">
 			<a>Markets</a>
-		</Link>
-		<Link href="/">
-			<a>Markets</a>
-		</Link>
-		<Link href="/">
+		</NavLink>
+		<NavLink href="/faq">
+			<a>FAQ</a>
+		</NavLink>
+		<NavLink href="/contact">
 			<a>Contact</a>
-		</Link>
+		</NavLink>
 	</StyledNav>
 )
 
