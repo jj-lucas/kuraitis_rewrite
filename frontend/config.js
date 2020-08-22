007ed488fbf495001c20818417504ada5b0d5695
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-08-22 18:14:34 +0200

    :wrench: Point to prod backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
 export const apiUrlDev = `http://localhost:4444`
-export const apiUrlProd = `http://localhost:4444`
+export const apiUrlProd = `http://164.90.221.92:4444`
