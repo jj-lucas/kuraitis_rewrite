ce4918a4a369e5d4643e175bbfc26e4d6dd376d7
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-09-12 11:58:06 +0200

    :sparkles: Implement user signup

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,34 @@
-const mutations = {};
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-module.exports = mutations;
+const mutations = {
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase()
+    // hash the passsword
+    const password = await bcrypt.hash(args.password, 10)
+    // create the user in the DB
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    )
+
+    // create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // set the JWT as a cooke on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+    })
+
+    // return the user to the browser
+    return user
+  },
+}
+
+module.exports = mutations
--- frontend/components/Homepage.js
+++ frontend/components/Homepage.js
@@ -31,6 +31,9 @@ const Homepage = props => {
 					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
 					est laborum.
 				</p>
+			</ContentSection>
+			<Slider slides={['hero2']} alt="Home page slider" />
+			<ContentSection>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
 					magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
