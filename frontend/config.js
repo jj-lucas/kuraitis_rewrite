68598f7f2673b185eb62580d7f65dae0e747333a
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-11-29 17:22:51 +0100

    :wrench: Update prod API endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
 export const apiUrlDev = `http://localhost:8000/graphql`
-export const apiUrlProd = `http://164.90.221.92:4444`
+export const apiUrlProd = `http://164.90.221.92:8000/graphql`
 export const cloudinaryUrl = `https://api.cloudinary.com/v1_1/kuraitis/image/upload`
 export const stripePublicKey = `pk_test_51GzQONHqFvOKDhGmsF50x344v3iwL4AY9faQGOQXAWokWrPXdtoja88f2pCRlN9yb0aFjRpo7mXfDwB5CVUL6See00GJkIQAKe`
 export const stripePaymentsUrl = `https://dashboard.stripe.com/test/payments/`
