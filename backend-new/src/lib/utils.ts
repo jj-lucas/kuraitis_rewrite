947286171462750fdb130c89595de4a5d8976389
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2020-12-29 12:04:16 +0100

    :sparkles: Split order confirmation template into sections

--- backend-new/src/lib/utils.ts
+++ backend-new/src/lib/utils.ts
@@ -1,4 +1,4 @@
-const makeMultiPrice = (dkk, conversionRates) => {
+export const makeMultiPrice = (dkk, conversionRates) => {
 	return {
 		DKK: dkk ? parseInt(dkk, 10) : null,
 		USD: dkk ? parseInt('' + parseInt(dkk, 10) * conversionRates.USD, 10) : null,
@@ -6,4 +6,17 @@ const makeMultiPrice = (dkk, conversionRates) => {
 		GBP: dkk ? parseInt('' + parseInt(dkk, 10) * conversionRates.GBP, 10) : null,
 	}
 }
-module.exports = makeMultiPrice
+
+export const prettyPrice = (amount, currency) => {
+	switch (currency) {
+		case 'DKK':
+			return amount ? `${amount},- DKK` : 'Gratis'
+		case 'USD':
+			return amount ? `${amount} $` : 'Free'
+		case 'EUR':
+			return amount ? `${amount} €` : 'Free'
+		case 'GBP':
+			return amount ? `${amount} £` : 'Free'
+	}
+	return ''
+}
