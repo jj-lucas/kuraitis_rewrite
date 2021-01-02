739a8cee8a3be0208c51eb0ceee9b0c44bc40c2c
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2021-01-02 13:52:15 +0100

    :sparkles: Extract selling points into its own component

---
+++ frontend/components/Site/SellingPoints.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react'
+import { FiTruck as DeliveryIcon } from 'react-icons/fi'
+import { ImGift as GiftIcon } from 'react-icons/im'
+import { MdCached as ReturnIcon, MdPinDrop as TrackIcon } from 'react-icons/md'
+import styled from 'styled-components'
+import { LocaleContext, translate } from '../../lib'
+
+const StyledSellingPoints = styled.div`
+	margin: 6rem 0 3rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-gap: 2rem;
+	text-align: center;
+`
+
+const SellingPoints = () => {
+	const { locale } = useContext(LocaleContext)
+
+	return (
+		<StyledSellingPoints>
+			<div>
+				<div>
+					<DeliveryIcon size={25} />
+				</div>
+				{locale === 'en' ? (
+					<p>
+						3-5 days until your order is made and shipped. <br />
+						Actual delivery time is not included. See the FAQ for more info.
+					</p>
+				) : (
+					<p>__selling_points_delivery</p>
+				)}
+			</div>
+			<div>
+				<div>
+					<GiftIcon size={25} />
+				</div>
+				{locale === 'en' ? <p>All items are shipped in gift packaging.</p> : <p>__selling_points_gift_wrapping</p>}
+			</div>
+			<div>
+				<div>
+					<ReturnIcon size={25} />
+				</div>
+				{locale === 'en' ? (
+					<p>
+						Free cancellations prior to shipping. <br />
+						30 days return right for non-customized products.
+					</p>
+				) : (
+					<p>__selling_points_cancellations_returns</p>
+				)}
+			</div>
+		</StyledSellingPoints>
+	)
+}
+
+export { SellingPoints }
