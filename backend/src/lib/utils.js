35d16805246a6eb3c7f1d25e35d8d7fc5068cc84
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2021-01-26 20:02:46 +0100

    :wrench: Remove old JS backend

--- backend/src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-const makeMultiPrice = (dkk, conversionRates) => {
-  return {
-    DKK: parseInt(dkk, 10) | null,
-    USD: parseInt(parseInt(dkk, 10) * conversionRates.USD, 10) | null,
-    EUR: parseInt(parseInt(dkk, 10) * conversionRates.EUR, 10) | null,
-    GBP: parseInt(parseInt(dkk, 10) * conversionRates.GBP, 10) | null,
-  }
-}
-module.exports = makeMultiPrice
